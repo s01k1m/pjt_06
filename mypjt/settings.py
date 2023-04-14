fa260ff4e0e42c3949f0fc015fc9a6e7d2ba7880
s0lk1m <sk618dev@gmail.com>
2023-04-14 13:32:25 +0900

    accounts model complete

--- mypjt/settings.py
+++ mypjt/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local apps
     'movies',
+    'accounts',
+    # basic apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +129,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+############################# 추가 ###################################
+
+
+AUTH_USER_MODEL = 'accounts.User'
