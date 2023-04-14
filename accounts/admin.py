33550dba1223262770f85393570b6a0c27ec239e
seoyoung81 <seosue0801@gmail.com>
2023-04-14 14:01:05 +0900

    url mapping

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+def delete(request):
+    pass
+
+def change_password(request):
+    pass
+
+def profile(request):
+    pass
+
+def follow(request):
+    pass
