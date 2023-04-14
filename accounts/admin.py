9584958da1a23f87cb35cbdd2f4978f21da0d7d0
s0lk1m <sk618dev@gmail.com>
2023-04-14 14:58:56 +0900

    accounts sol part

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
-
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
+# # Register your models here.
+# admin.site.register(User, UserAdmin)
 
 def delete(request):
     pass
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('delete/', views.delete, name='delete'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('update/', views.update, name="update"),
 ]
